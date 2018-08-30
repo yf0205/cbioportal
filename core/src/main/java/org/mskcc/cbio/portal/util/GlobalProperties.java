9944e33d6e1c8ef545a344a8daf406300b078eea
aaronlisman <lismanBusiness@gmail.com>
2018-08-30 13:57:30 -0400

    Adaptations for integration of SPA

--- core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
@@ -807,7 +807,7 @@ public class GlobalProperties {
         }
         
     }
- 
+    
     public static String getOncoKBApiUrl()
     {
         String oncokbApiUrl = portalProperties.getProperty(ONCOKB_API_URL);
